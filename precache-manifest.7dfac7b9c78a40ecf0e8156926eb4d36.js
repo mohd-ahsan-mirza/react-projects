07207a18de29e4aeeff423616d65efc63a47abc6
Muhammad Mirza <mohd.ahsan.mirza@gmail.com>
2019-05-06 11:34:00 -0400

    Updates

---
+++ precache-manifest.7dfac7b9c78a40ecf0e8156926eb4d36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e61d73e69813352cd9f0",
+    "url": "/list-manipulator/static/css/main.f19ce9c8.chunk.css"
+  },
+  {
+    "revision": "e61d73e69813352cd9f0",
+    "url": "/list-manipulator/static/js/main.e61d73e6.chunk.js"
+  },
+  {
+    "revision": "188f325f0d25fd11a60c",
+    "url": "/list-manipulator/static/js/runtime~main.188f325f.js"
+  },
+  {
+    "revision": "aa541ba5f659f38b577e",
+    "url": "/list-manipulator/static/css/2.fcdf11a1.chunk.css"
+  },
+  {
+    "revision": "aa541ba5f659f38b577e",
+    "url": "/list-manipulator/static/js/2.aa541ba5.chunk.js"
+  },
+  {
+    "revision": "9849d2ab8953457a96a3ca966a8dcf80",
+    "url": "/list-manipulator/index.html"
+  }
+];
