fb84b80008c82f880d4358cc46b5e0b73534e159
Ahsan <mohd.ahsan.mirza@gmail.com>
2019-08-25 09:42:32 -0400

    Bug fix

--- cron-graph-plotter/src/components/Cron.js
+++ cron-graph-plotter/src/components/Cron.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Graph from './Graph';
 import {Line} from 'react-chartjs-2';
+import Scatter from 'react-chartjs-2';
 
 class Cron extends Graph{
 	constructor(props){
@@ -17,10 +18,10 @@ class Cron extends Graph{
 		  };
 		
 		var crons = [
-					 {cron_label:"upload", cron_expression: parser.parseExpression('19-20 6 * * *',cron_options)},
-					 {cron_label: "cancel", cron_expression: parser.parseExpression('20 7 * * *',cron_options)},
-					 {cron_label: "notification", cron_expression: parser.parseExpression('0 8-9 * * *',cron_options)},
-					 {cron_label: "process", cron_expression: parser.parseExpression('*/10 0 * * *',cron_options)},
+					 //{cron_label:"upload", cron_expression: parser.parseExpression('19-20 6 * * *',cron_options)},
+					 //{cron_label: "cancel", cron_expression: parser.parseExpression('20 7 * * *',cron_options)},
+					 //{cron_label: "notification", cron_expression: parser.parseExpression('0 8-9 * * *',cron_options)},
+					 {cron_label: "process", cron_expression: parser.parseExpression('* 0 * * *',cron_options)},
 					];
 		
 		var dataSetArray= []
@@ -30,7 +31,7 @@ class Cron extends Graph{
 			var color = this.random_rgba()
 
 			var dataSetObject = {}
-			dataSetObject.type = 'line'
+			dataSetObject.type = ''
 			dataSetObject.label = crons[run].cron_label
 			dataSetObject.fill = false
 			dataSetObject.lineTension = 0.1
@@ -70,7 +71,8 @@ class Cron extends Graph{
 						},
 						distribution: 'series',
 					}]
-				}
+				},
+				responsive: true,
 			}
 			dataSetArray.push(dataSetObject)
 		}
@@ -88,11 +90,20 @@ class Cron extends Graph{
 			chartOptions:{
     			scales: {
         			yAxes: [{
-                        display: true,
-						},
-                    ],
+						stacked: false,
+  						ticks: {
+    						beginAtZero: true,
+    						min: 0,
+							max: 60
+						}
+					}],
 					xAxes: [{
-						display: true,
+						stacked: false,
+  						ticks: {
+    						beginAtZero: true,
+    						min: 0,
+							max: 24
+						}
         			}]
 				},
 				tooltips: {
